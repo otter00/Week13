c3aadbeeff76aa5e5347add1f286a0adeaf21868
otter00 <ivlevanastia2001@mail.ru>
2022-12-09 23:53:04 +0300

    index js changes with comments

--- Week 14/gallery/index.js
+++ Week 14/gallery/index.js
@@ -6,14 +6,15 @@ var images = document.querySelectorAll('.photo img');
 var i = 0;
 
 btn_prev.onclick = function(){
-    images[i].className = "";
-    i = i - 1; //i--
-    if( i < 0){
-        i = images.length - 1;  
+    images[i].className = ""; //0-element in a row
+    i = i - 1; //i-- - -1-element in a row (before 0 elm)
+    if(i < 0){ //if the number of element < 0, continue step back
+        i = images.length - 1; //length means number of img element in document
     }
-    images[i].className = "shown";
+    images[i].className = "shown"; //sh0w the elm
 };
 
+//the same as above, but step forward
 btn_next.onclick = function(){
     images[i].className = "";
     i = i + 1; //i++
