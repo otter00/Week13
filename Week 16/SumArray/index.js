b3b6a247aa4d259000117d78ffb332a70bdda978
otter00 <ivlevanastia2001@mail.ru>
2022-12-22 20:17:46 +0300

    array template

---
+++ Week 16/SumArray/index.js
@@ -0,0 +1,13 @@
+// const parser = (str) => {
+//     const arr = [];
+//     const parts = str.split('-');
+//     // На всякий случай делаем проверку, чтобы from было меньше to
+//     const from = Math.min(parseInt(parts[0]), parseInt(parts[1]));
+//     const to = Math.max(parseInt(parts[0]), parseInt(parts[1]));
+//     for(let i = from; i <= to; i++){
+//         arr.push(i);
+//     }    
+//     return arr;
+//  } 
+//  // Проверяем
+//  console.log(parser('10-22'));
