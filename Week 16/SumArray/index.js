00ecfdfff1bffcf2d5e0d976328f3ec80aded1d6
otter00 <ivlevanastia2001@mail.ru>
2022-12-22 21:10:28 +0300

    push values into array

--- Week 16/SumArray/index.js
+++ Week 16/SumArray/index.js
@@ -11,3 +11,38 @@
 //  } 
 //  // Проверяем
 //  console.log(parser('10-22'));
+
+
+const jsAdd = document.querySelector('.js-add');
+const addBtn = document.querySelector('.add');
+const resBtn = document.querySelector('.result');
+let Array = [];
+let sortedArray = [];
+
+//if we have event CLICK then enable function
+//pushes value into array
+addBtn.addEventListener('click', () => {
+    if(!jsAdd.value == "") {
+        //if value exists
+        Array.push(Number(jsAdd.value));
+    } else {
+        console.log('not a number');
+    }
+    jsAdd.value = '';
+
+    //push array into console
+    console.log(Array);
+});
+
+resBtn.addEventListener('click', () => {
+    for(let i = 0; i < Array.length; i++)
+    {
+        sortedArray.push(Array[i]);
+    }
+    // sortedArray.concat(Array);
+    // sortedArray.sort();
+    //Array.sort();
+    //sortedArray.sort();
+    console.log(sortedArray.sort());
+    sortedArray = [];
+});
