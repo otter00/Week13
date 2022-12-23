6fcb22a6a033f3cce5368a2effe151c536fb1c7b
otter00 <ivlevanastia2001@mail.ru>
2022-12-23 19:20:34 +0300

    3rd task from week16

--- Week 16/lesson/index.js
+++ Week 16/lesson/index.js
@@ -24,3 +24,22 @@
 // })
 
 // arr[2](); // ?
+
+
+//1. Вывести на экран первый элемент массива
+// const arr =  ['js', 'css', 'html'];
+// alert(arr[0]);
+
+//2. Отфильтруйте массив [0, 1, false, 2, undefined, '', 3, null] от нежелательных значений,
+//таких как false, undefined, пустые строки, 0, null с помощью метода filter.  
+//Ожидаемый результат: [1, 2, 3].
+//const arr = [0, 1, false, 2, undefined, '', 3, null];
+//  1ST WAY
+//const filteredArr = arr.filter(el=>(typeof el === "number" && el !== 0));
+//  2ND WAY
+//const filteredArr = arr.filter(Number);
+//alert(filteredArr);
+
+//3. Дан массив [[1,2], [1,2,3], [1,2,3,4]]. 
+//Найдите индекс массива, длина которого > 3. Ожидаемый результат: 2
+const arr = [[1,2], [1,2,3], [1,2,3,4]];
