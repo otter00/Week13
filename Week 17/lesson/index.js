7bd49b752865cf442d75447eff594221bb4af269
otter00 <ivlevanastia2001@mail.ru>
2022-12-28 13:02:38 +0300

    lesson tasks (w17)

---
+++ Week 17/lesson/index.js
@@ -0,0 +1,21 @@
+// 1. Напишите код, который делает первый символ заглавным. 
+//первый способ
+// let first = `настя`;
+// let up = first.replace(first[0], first[0].toUpperCase());
+// console.log(up);
+// let second = first[0].toUpperCase();
+// let third = first.replace(first[0], second);
+//второй способ
+// const name = "john"
+// const result = name.charAt(0).toUpperCase() + name.slice(1)
+// console.log(result);
+
+// 2. Создать дату 24 января 2021 года, 22 часа 51 минута. Временная зона – местная.
+// let date = new Date(2021, 0, 24, 22, 51);
+// console.log( date ); // 2021-01-24, 22:51:00
+
+// 3. Посчитать, сколько секунд осталось до завтра.
+// let today = new Date();
+// let tomorrow = new Date(2022, 11, 29, 0); // 11 месяц - потому что у timestamp всё не как у людей
+// let diff = tomorrow.getTime() - today.getTime();
+// console.log( diff/1000/3600 );
