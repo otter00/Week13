27cdfcd19e260b83aa172fc978f09e507512823b
otter00 <ivlevanastia2001@mail.ru>
2022-12-29 14:12:46 +0300

    lesson task 1st (w17)

--- Week 17/inputs/index.js
+++ Week 17/inputs/index.js
@@ -1,23 +1,53 @@
 let mfield = document.querySelector('.mfield');
-let fname = document.querySelector('.fname');
-let sname = document.querySelector('.sname');
-let lname = document.querySelector('.lname');
+// let fname = document.querySelector('#fname');
+// let sname = document.querySelector('#sname');
+// let lname = document.querySelector('#lname');
 let button = document.querySelector('#splitted');
 
+let inp = document.querySelectorAll('.inp');
+console.log(inp);
+//console.log(inp.length);
+
 let trimmed;
+let letters;
 let msplitted;
 
 button.addEventListener('click', ()=>{
-    msplitted = mfield.value.split(" ");
+    console.log(mfield.value); //check whether the field is ok
+
+    //letters = mfield.value.match(/[a-z]/gi).join(" ");
+    //console.log(letters);
+
+    console.log(typeof(mfield.value)); //string
+
+    msplitted = 
+        mfield.value.split(" ").filter(
+        el=>(el !== "")).map(
+            //toLowerCase the entire word
+            (word, index) => word.toLowerCase()).map(
+        //split the string into array
+        //filter the array
+        //toUpperCase the first letter
+        (word, index) => word[0].toUpperCase() + word.slice(1));
     console.log(msplitted);
 
-    for(let i=0; i <= msplitted.length; i++) {
-        fname.setAttribute('value', `${msplitted[0]}`);
-        sname.setAttribute('value', `${msplitted[1]}`);
-        lname.setAttribute('value', `${msplitted[2]}`);
-        
-        //document.querySelector('.fname').innerHTML = msplitted[0];
-        //document.querySelector('.sname').innerHTML = msplitted[1];
-        //document.querySelector('.lname').innerHTML = msplitted[2];
+    //trimmed = msplitted.filter(el=>(el !== "")); //filter the array
+    //console.log(trimmed);
+
+    for (let i=0; i <= inp.length; i++) {
+        inp[i].setAttribute('value', `${msplitted[i]}`); //set values into their places
     }
+    // for (let i=0; i <= inp.length; i++) {
+    //     inp[i].value.match(/[a-z]/gi).join('');
+    // }
+
+    // for(let i=0; i <= msplitted.length; i++) {
+    //     fname.setAttribute('value', `${msplitted[0]}`);
+    //     sname.setAttribute('value', `${msplitted[1]}`);
+    //     lname.setAttribute('value', `${msplitted[2]}`);
+        
+    //     //document.querySelector('.fname').innerHTML = msplitted[0];
+    //     //document.querySelector('.sname').innerHTML = msplitted[1];
+    //     //document.querySelector('.lname').innerHTML = msplitted[2];
+    // }
 });
--- Week 17/lesson/index.js
+++ Week 17/lesson/index.js
@@ -21,12 +21,11 @@
 // console.log( diff/1000/3600 );
 
 // 4. Выделить из строки с денежной суммой (например, 120р. или 99€) только цифры, т.е. 120 и 99
-// let currency = `120p.`;
-// let currencyEur = `99€`;
-// console.log(currency);
-// console.log(+currency.slice(0, -2));
-// console.log(currency.match(/[0-9]/gi).join(''));
-// console.log(currencyEur.match(/[0-9]/gi).join(''));
+//let currency = `120p.`;
+//let currencyEur = `99€`;
+//console.log(currency);
+//console.log(currency.match(/[0-9]/gi).join(''));
+//console.log(currencyEur.match(/[0-9]/gi).join(''));
 
 // 5. Сгенерировать случайное число от 1 до 100
 // let rand = Math.round((Math.random()*101));
