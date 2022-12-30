1b98423615f3676bb752d0521ed20423f0eff346
otter00 <ivlevanastia2001@mail.ru>
2022-12-30 18:23:44 +0300

    checkSpam create div by js methods (w17)

--- Week 17/checkSpam/index.js
+++ Week 17/checkSpam/index.js
@@ -3,8 +3,14 @@ let field = document.querySelector('#comment__field');
 let commentContent = document.querySelector('#content');
 
 function checkSpam() {
+    let elemDiv = document.createElement('div');
+    elemDiv.style.cssText = 'width:20vw;min-height:50px;margin-left:1%;border:1px solid black;border-radius:7px;';
+    document.body.appendChild(elemDiv);
+
+    elemDiv.innerHTML += `${field.value.replace(/viagra/i, '***').replace(/xxx/i, '***').replace(/innocent rabbit/i, '***')}\n`
+    
     //field.value.replace(/viagra/i, '***').replace(/xxx/i, '***').replace(/innocent rabbit/i, '***');
-    commentContent.innerHTML += `${field.value.replace(/viagra/i, '***').replace(/xxx/i, '***').replace(/innocent rabbit/i, '***')}\n`
+    //commentContent.innerHTML += `${field.value.replace(/viagra/i, '***').replace(/xxx/i, '***').replace(/innocent rabbit/i, '***')}\n`
     //console.log(field.value.replace(/viagra/i, '***').replace(/xxx/i, '***').replace(/innocent rabbit/i, '***'));
 }
 
