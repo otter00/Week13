b8090066616ec79fbb2e3463ba59829665323695
otter00 <ivlevanastia2001@mail.ru>
2022-12-30 15:26:34 +0300

    checkSpam completed (w17)

--- Week 17/checkSpam/index.js
+++ Week 17/checkSpam/index.js
@@ -2,13 +2,17 @@ let addButton = document.querySelector('#add__comment');
 let field = document.querySelector('#comment__field');
 let commentContent = document.querySelector('#content');
 
-// function checkSpam(field) {
-// }
-
-addButton.addEventListener('click', ()=>{
+function checkSpam() {
+    //field.value.replace(/viagra/i, '***').replace(/xxx/i, '***').replace(/innocent rabbit/i, '***');
+    commentContent.innerHTML += `${field.value.replace(/viagra/i, '***').replace(/xxx/i, '***').replace(/innocent rabbit/i, '***')}\n`
     //console.log(field.value.replace(/viagra/i, '***').replace(/xxx/i, '***').replace(/innocent rabbit/i, '***'));
-    commentContent.innerHTML += `${field.value.replace(/viagra/i, '***').replace(/xxx/i, '***').replace(/innocent rabbit/i, '***')}\n`;
-});
+}
+
+// addButton.addEventListener('click', ()=>{
+//     //console.log(field.value.replace(/viagra/i, '***').replace(/xxx/i, '***').replace(/innocent rabbit/i, '***'));
+//     //commentContent.innerHTML += `${field.value.replace(/viagra/i, '***').replace(/xxx/i, '***').replace(/innocent rabbit/i, '***')}\n`;
+//     //commentContent.innerHTML += `${field.value}\n`
+// });
 
 addButton.addEventListener('click', ()=>{
     field.value = "";
