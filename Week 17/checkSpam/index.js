2e6dba9c9025a15be5086598c3e7bac585330b17
otter00 <ivlevanastia2001@mail.ru>
2022-12-30 14:47:38 +0300

    checkSpam func added (w17)

--- Week 17/checkSpam/index.js
+++ Week 17/checkSpam/index.js
@@ -2,8 +2,12 @@ let addButton = document.querySelector('#add__comment');
 let field = document.querySelector('#comment__field');
 let commentContent = document.querySelector('#content');
 
+// function checkSpam(field) {
+// }
+
 addButton.addEventListener('click', ()=>{
-    commentContent.innerHTML += `${field.value}\n`;
+    //console.log(field.value.replace(/viagra/i, '***').replace(/xxx/i, '***').replace(/innocent rabbit/i, '***'));
+    commentContent.innerHTML += `${field.value.replace(/viagra/i, '***').replace(/xxx/i, '***').replace(/innocent rabbit/i, '***')}\n`;
 });
 
 addButton.addEventListener('click', ()=>{
