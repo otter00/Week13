49bf9bf16d88db6ee62612b40be33b9d58ecbc86
otter00 <ivlevanastia2001@mail.ru>
2023-01-14 18:31:49 +0300

    structure (w18)

---
+++ Week 18/local storage with pic/index.js
@@ -0,0 +1,11 @@
+//get each input
+const formName = document.querySelector('.form__name');
+const formAva = document.querySelector('.form__ava');
+const formText = document.querySelector('.form__text');
+
+//get the send button
+const formButton = document.querySelector('.form__button');
+
+//get the field for comments
+const listComments = document.querySelector('.list__comments');
+
