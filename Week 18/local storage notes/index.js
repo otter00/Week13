8c9542f18b731f3f41a20616617302b625c4964e
otter00 <ivlevanastia2001@mail.ru>
2023-01-21 13:16:14 +0700

    localstorage + objects (w18, w19)

--- Week 18/local storage notes/index.js
+++ Week 18/local storage notes/index.js
@@ -48,19 +48,33 @@ const delValue = (input) => {
     input.value = ''
 }
 
+//get all the keys of object (localstorage is object)
+const allKeys = Object.keys(localStorage);
+for (const key of allKeys) {
+    console.log(key);
+    console.log(localStorage[key]);
+}
 
+//делаем проверку, есть ли заметки в локальном хранилище. Внимание! Этот материал вы еще не проходили, это уже работа с объектами. Усложненная часть задачи
+if (localStorage.length != 0) {
+    const arrKeys = Object.keys(localStorage);
+    //переписала функцию без получения методов. Расскажу на ближайших уроках почему не получилось в первый раз и почему сработает этот способ. Костыль убрала! 
+    arrKeys.forEach((key) => {
+        addComment(listComments, createCard(localStorage.getItem(key), key))
+    })
+}
 //look whether local storage already have saved notes
 
 //Внимание! Этот материал вы еще не проходили, это уже работа с объектами. 
 //Усложненная часть задачи
 
-if (localStorage.length != 0) {
-    for (key in localStorage) {
-        if (key[0] == 1) {
-            addComment(listComments, createCard(localStorage.getItem(key), key));
-        }
-    }
-}
+// if (localStorage.length != 0) {
+//     for (key in localStorage) {
+//         if (key[0] == 1) {
+//             addComment(listComments, createCard(localStorage.getItem(key), key));
+//         }
+//     }
+// }
 
 //check whether cotent field is empty
 let checkContent = (note) => {
@@ -83,7 +97,7 @@ const handleComment = () => {
     if (!checking) {
         alert("Type the note first");
     } else {
-        saveComment(`1${date}`, comment.value);
+        saveComment(`${date}`, comment.value);
         addComment(listComments, createCard(comment.value, date));
         delValue(comment);
     }
--- Week 19/lesson/index.js
+++ Week 19/lesson/index.js
@@ -40,3 +40,56 @@
 // console.log(person); //name, age
 // console.log(person.city); //undefined
 // console.log(city); //amsterdam
+
+//objects assigning
+// const obj1 = { a: 'a', b: 'b'};
+// console.log(obj1);
+// const obj2 = { c: 'c', b: 'v'};
+// console.log(obj2);
+// Object.assign(obj1, obj2);
+//console.log(obj1);
+
+// const obj1= { a: { a: 3 } }
+// console.log(obj1);
+// const obj2= { a: { b: 1 } }
+// console.log(obj2);
+// Object.assign(obj1, obj2);
+// console.log(obj1);
+
+// const obj1= { a: { a: { c: 3 } } }
+// const copyObj1 = { ... obj1}; //spread
+// console.log(obj1);
+// console.log(copyObj1);
+// console.log(copyObj1 == obj1); //false, links are not equal
+// console.log(copyObj1.a == obj1.a); //true, 'cause obj a is equal to both (link to the same internal object)
+// const obj2= { a: { b: 1 } }
+// console.log(obj2);
+// Object.assign(obj1, obj2);
+// console.log(obj1);
+
+// const aircraft = {
+//     type: 'boeing',
+//     number: 747,
+//     altitude: 12000,
+//     //distance: 12000,
+// };
+// const copyType = aircraft.type; //simple copying
+// const {type, number, altitude, distance = 15000} = aircraft;
+// console.log(distance); //just a variable now
+// console.log(aircraft);
+
+// const func = () => {
+//     console.log(type);
+// }
+// //func.width = 200;
+// //console.log(func); functions is an object
+// const array = [1, 4, 8]; //index 0 1 2
+// array[5] = 6;
+// console.log(array);
+
+// const user= { name: 'Tirion', email: 'support@hexlet.io', age: 44 };
+// const { name, ...rest }= user;
+// const { ...rest2 }= user;
+// console.log(rest);
+// console.log(rest2);
+// => { email: 'support@hexlet.io', age: 44 }
