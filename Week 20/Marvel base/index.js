3265b513e476bec69846612332132513fe6b424b
otter00 <ivlevanastia2001@mail.ru>
2023-01-25 16:14:01 +0700

    raiting for cards (w20)

--- Week 20/Marvel base/index.js
+++ Week 20/Marvel base/index.js
@@ -106,7 +106,21 @@ document.addEventListener('DOMContentLoaded', function (e) {
         `<span><p class='bold__text'>Суперспособности:</p> ${card.superpowers}</span>` + 
         `<div><img src='${card.url}'></div>` + 
         `<p>${card.info}</p>
-        </div>`;
+
+            <div class="rating-area">
+        <input type="radio" id="star-5" name="rating" value="5">
+        <label for="star-5" title="Оценка «5»"></label>	
+        <input type="radio" id="star-4" name="rating" value="4">
+        <label for="star-4" title="Оценка «4»"></label>    
+        <input type="radio" id="star-3" name="rating" value="3">
+        <label for="star-3" title="Оценка «3»"></label>  
+        <input type="radio" id="star-2" name="rating" value="2">
+        <label for="star-2" title="Оценка «2»"></label>    
+        <input type="radio" id="star-1" name="rating" value="1">
+        <label for="star-1" title="Оценка «1»"></label>
+            </div>
+        </div>`
     }
+
     document.getElementById('gallery__container').innerHTML = cardsContent;
 });
