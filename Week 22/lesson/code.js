273280746ff0a7c89859dd063b054ec72fd067fe
otter00 <ivlevanastia2001@mail.ru>
2023-02-07 20:43:03 +0300

    try-catch-finally (w22)

---
+++ Week 22/lesson/code.js
@@ -0,0 +1,13 @@
+let val = 'sss';
+
+try {
+    let numVal = Number(val);
+    if(val !== numVal) {
+        throw new Error("Not a number!");
+    }
+    console.log(parseInt(val));
+} catch (e) {
+    console.log(e.message);
+} finally {
+    console.log(`I'm done`);
+}
